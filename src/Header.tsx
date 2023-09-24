f9bb07f9e2c988ab26f99b1a515cf252d8a82e2b
muthu12-sudo <muthuganeshrmg2205@gmail.com>
2023-09-24 08:55:34 +0530

    Initial commit

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import "./Header.css";
 
 function Header() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <div className="header">
         <h1>Hello Everyone</h1>
+        <br />
+        <button
+          className="btn btn-secondary"
+          onClick={() => setCount(count + 1)}>
+          Welcome {count}
+        </button>
       </div>
     </>
   );
