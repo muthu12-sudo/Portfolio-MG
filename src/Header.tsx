ddb8c1cfdd53e33ca47984d069c30394f27adeb4
muthu12-sudo <muthuganeshrmg2205@gmail.com>
2023-09-24 14:10:09 +0530

    Dynamic Page

--- src/Header.tsx
+++ src/Header.tsx
@@ -7,7 +7,15 @@ function Header() {
   return (
     <>
       <div className="header">
-        <h1>Hello Everyone</h1>
+        <h1>
+          This is Dynamic{" "}
+          <span>
+            <img src="/src/assets/react.svg" />
+          </span>{" "}
+          React Page
+        </h1>
+        <img id="mg" src="/Cartoon-MG.png" width="300px" />
+        <h1>Welcome</h1>
         <br />
         <button
           className="btn btn-secondary"
