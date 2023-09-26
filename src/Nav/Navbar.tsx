48c6d9f182aebc0705b53ea3046d65a76a12a11c
muthu12-sudo <muthuganeshrmg2205@gmail.com>
2023-09-26 19:07:46 +0530

    NavBar

--- src/Nav/Navbar.tsx
+++ src/Nav/Navbar.tsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import "./styles.css";
+
 export default function Navbar() {
+  const [navbarWidth, setNavbarWidth] = useState("0%");
+
+  const handleScroll = () => {
+    const scrollY = window.scrollY;
+    const PageTotalWidth =
+      document.documentElement.scrollHeight - window.innerHeight;
+    const percentageScrolled = (scrollY / PageTotalWidth) * 100;
+
+    const newWidth = Math.min(Math.max(percentageScrolled, 0), 100) + "%";
+    setNavbarWidth(newWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <div className="Navbar">
-        <h1>Muthuganesh R</h1>
-        <h1>About</h1>
-        <h1>Projects</h1>
-        <h1>Education</h1>
-        <hr />
+      <div className="NavbarContainer">
+        <div className="Navbar">
+          <h1>Muthuganesh R</h1>
+          <span>About</span>
+          <span>Projects</span>
+          <span>Education</span>
+        </div>
+        <hr style={{ width: navbarWidth }} />
       </div>
     </>
   );
