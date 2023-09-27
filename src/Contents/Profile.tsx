43ae979f915ba44eef17da88c0003b278a01d533
muthu12-sudo <muthuganeshrmg2205@gmail.com>
2023-09-27 18:57:10 +0530

    Add Flip Card

--- src/Contents/Profile.tsx
+++ src/Contents/Profile.tsx
@@ -1,11 +1,13 @@
 import cartoonmg from "/Cartoon-MG.png";
+import MGImg from "/Mg-Img.jpg";
+import "../stylesheet.css";
 export default function Profile() {
   return (
     <>
       <div className="Profile">
         {" "}
         <div id="mgimg">
-          <img src={cartoonmg} width="300px" />
+          <img src={MGImg} width="300px" />
         </div>
         <div className="ProfileContainer">
           <p>
@@ -15,6 +17,37 @@ export default function Profile() {
             occaecat aliquip consectetur. Adipisicing aute Lorem ea laborum enim
             consequat excepteur aliquip cillum anim.
           </p>
+        </div>
+      </div>
+      <div className="artboard">
+        <div className="card">
+          <div className="card__side card__side--back">
+            <div className="card__cover">
+              <h4 className="card__heading">
+                <span className="card__heading-span">Skill Set</span>
+              </h4>
+            </div>
+            <div className="card__details">
+              <ul>
+                <li>Advanced JS and CSS</li>
+                <li>JS/CSS Preprocessors</li>
+                <li>JS Frameworks</li>
+                <li>Advanced Animations</li>
+                <li>Deployment Pipelines</li>
+                <li>Large Apps Architectures</li>
+                <li>Naming Conventions</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="card__side card__side--front">
+            <div className="card__theme">
+              <div className="card__theme-box">
+                <p className="card__subject">Muthuganesh</p>
+                <p className="card__subject">Hello World!</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
