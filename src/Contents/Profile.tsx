6592724d5bad3dcadeededaa10041747dbdfa20e
muthu12-sudo <muthuganeshrmg2205@gmail.com>
2023-09-28 14:17:31 +0530

    Profile Content Complete

--- src/Contents/Profile.tsx
+++ src/Contents/Profile.tsx
@@ -10,11 +10,14 @@ export default function Profile() {
         <div className="ProfileContainer">
           <h1>Hello There </h1>
           <p>
-            &ensp; &ensp; &ensp; &ensp; &ensp; I am Muthuganesh, from
-            Tirunelveli, Tamil Nadu, India. I am seeking an entry-level job
-            while concurrently pursuing my Master's degree. I have excellent
-            skills in web development and software development, and I have
-            worked on several projects as a technical head and team leader.
+            &ensp; &ensp; &ensp;I am Muthuganesh, from Tirunelveli, Tamil Nadu,
+            India. I am seeking an entry-level job while concurrently pursuing
+            my Master's degree. I have excellent skills in web development and
+            software development, and I have worked on several projects as a
+            technical head and team leader. I am eagerly waiting to build my
+            career and contribute my knowledge to my future organization. All I
+            need is an opportunity to showcase my skills. Kindly contact me if
+            you are interested in hiring me.
           </p>
         </div>
       </div>
